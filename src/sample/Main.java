36deddc20110be5bc713e0bef31112bcd8a6d1e4
Kirill_Gigante <kdgigante@yandex.ru>
2021-02-24 19:29:01 +0300

    Homework completes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("New chart");
-        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.setScene(new Scene(root, 900, 490));
         primaryStage.show();
     }
 
