e2aabde24312bdd6417b6b1bbbbff94a36b41b5e
Kirill_Gigante <kdgigante@yandex.ru>
2021-02-24 19:33:17 +0300

    Homework completed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,15 +6,4 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    public javafx.scene.control.TextArea TextArea;
-    @FXML
-    public javafx.scene.control.TextField TextField;
-
-
-    public void sendMsg(ActionEvent actionEvent) {
-        TextArea.appendText(TextField.getText()+"\n");
-        TextField.clear();
-        TextField.requestFocus();
-    }
 }
